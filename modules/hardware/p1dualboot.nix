5cdcbd7638b0d523e5869ee192df3799d95c2919
Apollo-sudo767 <fireshifter767@gmail.com>
2026-03-24 12:14:20 -0500

    holy hell

--- modules/hardware/p1dualboot.nix
+++ modules/hardware/p1dualboot.nix
@@ -13,9 +13,10 @@ in
   config = lib.mkIf cfg.enable {
     boot.loader.limine = {
       extraEntries = ''
-        :Windows
-        protocol: efi_chainload
-        image_path: boot():/EFI/Microsoft/Boot/bootmgfw.efi
+        +Windows
+            protocol: efi_chainload
+            # We add the /EFI/ prefix explicitly
+            image_path: boot():/EFI/Microsoft/Boot/bootmgfw.efi
       '';
     };
   };

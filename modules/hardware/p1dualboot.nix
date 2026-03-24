8467c660fe28efa10234825b36cd256501432ffe
Apollo-sudo767 <fireshifter767@gmail.com>
2026-03-24 12:03:14 -0500

    Hoping and praying

--- modules/hardware/p1dualboot.nix
+++ modules/hardware/p1dualboot.nix
@@ -15,7 +15,7 @@ in
       extraEntries = ''
         :Windows
         protocol: efi_chainload
-        image_path: uuid://FA0B-D29A/EFI/Microsoft/Boot/bootmgfw.efi
+        image_path: boot():/EFI/Microsoft/Boot/bootmgfw.efi
       '';
     };
   };

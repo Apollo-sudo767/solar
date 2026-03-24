2a61f352a1cbdecce1500044a809673cb839070d
Apollo-sudo767 <fireshifter767@gmail.com>
2026-03-24 11:56:50 -0500

    doing suff

--- modules/hardware/p1dualboot.nix
+++ modules/hardware/p1dualboot.nix
@@ -1,20 +1,9 @@
-{ config, lib, pkgs, inputs, ... }:
-
-let
-  cfg = config.myFeatures.hardware.p1dualboot;
-in
-{
-  options.myFeatures.hardware.p1dualboot = {
-    enable = lib.mkEnableOption "Enables Windows Dualboot on Mars";
-  };
-
-  config = lib.mkIf cfg.enable {
+config = lib.mkIf cfg.enable {
     boot.loader.limine = {
       extraEntries = ''
         :Windows
-         protocol: efi_chainload
-         image_path: uuid://FA0B-D29A/EFI/Microsoft/Boot/bootmgfw.efi
+        protocol: efi_chainload
+        image_path: uuid://FA0B-D29A/EFI/Microsoft/Boot/bootmgfw.efi
       '';
     };
   };
-}

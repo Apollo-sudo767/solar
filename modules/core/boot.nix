8bb42ba2fcbb8abb00ecb60bd92f79a2d5601efc
Apollo-sudo767 <blured767@gmail.com>
2026-03-23 19:02:37 -0500

    removed boot arguments

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -28,6 +28,8 @@ in
       };
     };
 
+    boot.kernelParams = [ "quiet" "loglevel=3" "systemd.show_status=auto" "rd.udev.log_level=3" ];
+    
     boot.plymouth.enable = true;
     # Essential system packages
     environment.systemPackages = with pkgs; [

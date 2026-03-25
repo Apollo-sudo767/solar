15823459b0b5defd151e683f5709c4cb61f1f9b0
Apollo-sudo767 <fireshifter767@gmail.com>
2026-03-24 23:25:46 -0500

    Finished battery, fully operational

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -11,7 +11,6 @@ in
   config = lib.mkIf cfg.enable {
     hardware.bluetooth = {
       enable = true;
-      powerOnBoot = true;
     };
     services.blueman.enable = true;
     environment.systemPackages = [ pkgs.bluez ];

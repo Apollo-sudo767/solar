8a55ea87903d57170417f9d242502b6c91041e49
Apollo-sudo767 <fireshifter767@gmail.com>
2026-03-29 20:02:35 -0500

    initial venus introduction

---
+++ hosts/venus/default.nix
@@ -0,0 +1,48 @@
+{ lib, inputs, ... }:
+
+{
+
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  networking.hostName = "mars";
+  system.stateVersion = "26.05";
+
+  myFeatures = {
+    core.enable = true;
+    shell.enable = true;
+    # core.secureboot.enable = true;
+    hardware = {
+      amd.enable = true;
+      dualboot.enable = true;
+      bluetooth.enable = true;
+      ttyResolution.enable = true;
+    };
+    systems = {
+      presets.gruvboxNiri.enable = true;
+      displayManager.manager = "tuigreet";
+    };
+    programs = {
+      ghostty.enable = true;
+      gaming.enable = true;
+      firefox.enable = true;
+      zen.enable = true;
+      fastfetch.enable = true;
+      helix.enable = true;
+      media.enable = true;
+      social.enable = true;
+      obs.enable = true;
+      stylePackages.enable = true;
+      bitwarden.enable = true;
+      davinci.enable = true;
+    };
+    services = {
+      audio.enable = true;
+      flatpak.enable = true;
+      printing.enable = true;
+      xdgPortals.enable = true;
+      udisks2.enable = true;
+    };
+  };
+}
---
+++ hosts/venus/settings.nix
@@ -0,0 +1,4 @@
+{
+  system = "x86_64-linux"; # or "aarch64-linux"
+  stable = false;          # Set to true if you want the 25.11 channel
+}

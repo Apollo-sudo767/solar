a5d580eeecb6c987cb11bf3c4ee92e825e1c8828
Apollo-sudo767 <blured767@gmail.com>
2026-04-04 23:01:07 -0500

    pluto password

--- hosts/pluto/default.nix
+++ hosts/pluto/default.nix
@@ -14,4 +14,6 @@
       anytypeSync.enable = true; # New feature toggle
     };
   };
+
+  users.users.apollo.hashedPassword = "$6$uEe7O.pykcuIaZVc$oCGnxu68r7wpVkOAhJiXC.TGDwE6kETEYQTWCl.Y1Vsxn21WgRkjsuv0sTXB0ygf1duIJytQ6h3TgLmjVuKbr/";
 }

4e704f6ffb6e73661dbc05b3198d29e209d7862e
Apollo-sudo767 <blured767@gmail.com>
2026-04-18 00:22:52 -0500

    si

--- modules/services/anytype-server.nix
+++ modules/services/anytype-server.nix
@@ -18,26 +18,31 @@ in
     
     virtualisation.oci-containers.containers = {
       anytype-sync = {
-        image = "docker.io/anyproto/any-sync-node:latest";
-        ports = [ "33010:33010" "33020:33020/udp" ];
-        volumes = [ "/var/lib/anytype/storage:/etc/any-sync" ];
+        image = "docker.io/anyproto/any-sync-node:latest"; # Use the image that worked!
+        ports = [
+          "33010:33010"
+          "33020:33020/udp"
+        ];
+        volumes = [
+          "/var/lib/anytype/storage:/etc/any-sync"
+        ];
         environment = {
           "ANY_SYNC_MONGODB_CONNECTION" = "mongodb://anytype-mongo:27017";
           "ANY_SYNC_REDIS_CONNECTION" = "redis://anytype-redis:6379";
         };
-        extraOptions = [ "--restart=always" ];
+        # REMOVE the --restart=always line here to fix the "conflict" error
         dependsOn = [ "anytype-mongo" "anytype-redis" ];
       };
 
       anytype-consensus = {
-        image = "docker.io/anyproto/any-sync-consensusnode:latest";
+        image = "docker.io/anyproto/any-sync-consensusnode:latest"; # Use the image that worked!
         ports = [ "3000:3000" ];
         environment = {
           "ANY_SYNC_MONGODB_CONNECTION" = "mongodb://anytype-mongo:27017";
           "ANY_SYNC_REDIS_CONNECTION" = "redis://anytype-redis:6379";
         };
       };
-
+            
       anytype-mongo = {
         image = "docker.io/library/mongo:latest";
         volumes = [ "/var/lib/anytype/mongo:/data/db" ];

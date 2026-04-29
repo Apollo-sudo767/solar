1b36402667f08069ae74eb88f150502f89dde3fe
Apollo-sudo767 <fireshifter767@gmail.com>
2026-04-29 13:31:20 -0500

    darkrpg

--- modules/services/servers/minecraft/darkrpg.nix
+++ modules/services/servers/minecraft/darkrpg.nix
@@ -36,7 +36,7 @@ in
 
       servers.darkRPG = {
         enable = true;
-        package = pkgs.minecraftServers.fabric-1_21_1;
+        package = pkgs.minecraftServers.fabric-1_20_1;
         jvmOpts = "-Xmx8G -Xms4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1";
 
         symlinks = {
@@ -53,7 +53,7 @@ in
           server-port = cfg.port;
           online-mode = true;
           enforce-secure-profile = false;
-          motd = "Solar | Dark RPG 1.21.1";
+          motd = "Solar | Dark RPG 1.20.1";
         };
       };
     };

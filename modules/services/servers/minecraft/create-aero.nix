6c78b11bd0b1d0a14bab970ecbdda9946caa5366
Apollo-sudo767 <fireshifter767@gmail.com>
2026-05-04 21:18:03 -0500

    create-aero fixing symlinks

--- modules/services/servers/minecraft/create-aero.nix
+++ modules/services/servers/minecraft/create-aero.nix
@@ -39,16 +39,16 @@ in
         package = pkgs.minecraftServers.neoforge-1_21_1;
         jvmOpts = "-Xmx12G -Xms4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1";
 
-        # Remove 'config' from symlinks to avoid read-only directory errors
+        # Fixed paths by adding 'pack-src'
         symlinks = {
-          "defaultconfigs" = "${modpack}/overrides/defaultconfigs";
-          "mods" = "${modpack}/mods"; # Ensure mods are linked
+          "defaultconfigs" = "${modpack}/pack-src/overrides/defaultconfigs";
+          "mods" = "${modpack}/mods";
         };
 
-        # Use 'files' to copy contents so the directory remains writable
+        # Fixed paths by adding 'pack-src'
         files = {
           "server-icon.png" = iconFile;
-          "config" = "${modpack}/overrides/config";
+          "config" = "${modpack}/pack-src/overrides/config";
         };
 
         serverProperties = {

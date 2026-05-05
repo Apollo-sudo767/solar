13715c794ebb02badf2cad3b7d669ea07f5df6be
Apollo-sudo767 <fireshifter767@gmail.com>
2026-05-04 21:23:01 -0500

    create-aero removing old files

--- modules/services/servers/minecraft/create-aero.nix
+++ modules/services/servers/minecraft/create-aero.nix
@@ -20,7 +20,7 @@ in
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
 
   options.myFeatures.services.servers.minecraft.create-aero = {
-    enable = lib.mkEnableOption "DarkRPG Minecraft 1.21.1 Fabric Modpack";
+    enable = lib.mkEnableOption "Create Aeronautics Minecraft 1.21.1 Neoforge Modpack";
     port = lib.mkOption {
       type = lib.types.port;
       default = 25565;
@@ -40,22 +40,20 @@ in
         jvmOpts = "-Xmx12G -Xms4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1";
 
         symlinks = {
-          # Mods can remain symlinked as they are read-only jars
           "mods" = "${modpack}/mods";
         };
 
         files = {
           "server-icon.png" = iconFile;
-          # MOVED: defaultconfigs must be a writable copy, not a symlink
-          "defaultconfigs" = "${modpack}/pack-src/overrides/defaultconfigs";
           "config" = "${modpack}/pack-src/overrides/config";
+          # REMOVED: "defaultconfigs" because it does not exist in this modpack
         };
 
         serverProperties = {
           server-port = cfg.port;
           online-mode = true;
           enforce-secure-profile = false;
-          motd = "PhasMC Create Aero 1.21.11";
+          motd = "PhasMC Create Aero 1.21.1";
         };
       };
     };

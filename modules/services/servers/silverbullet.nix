cb43c24a5b4f86272d7871888fc6a445a3eb56b2
Apollo-sudo767 <blured767@gmail.com>
2026-05-31 11:42:45 -0500

    fix(silverbullet): add proxy headers and base url

--- modules/services/servers/silverbullet.nix
+++ modules/services/servers/silverbullet.nix
@@ -21,18 +21,29 @@ in
       default = "sb.apollan.cc";
       description = "Domain for SilverBullet.";
     };
+    authTokenFile = lib.mkOption {
+      type = lib.types.nullOr lib.types.path;
+      default = "/var/lib/secrets/silverbullet-auth";
+      description = "Path to a file containing the SB_AUTH_TOKEN.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     services.silverbullet = {
       enable = true;
       listenPort = cfg.port;
-      listenAddress = "0.0.0.0";
+      listenAddress = "127.0.0.1"; # Only listen on localhost as we use Nginx
       spaceDir = "/var/lib/silverbullet";
     };
 
-    systemd.services.silverbullet.serviceConfig.Environment =
-      "SB_CHROME_PATH=${pkgs.chromium}/bin/chromium";
+    systemd.services.silverbullet.serviceConfig = {
+      Environment = [
+        "SB_CHROME_PATH=${pkgs.chromium}/bin/chromium"
+        "SB_BASE_URL=https://${cfg.domain}"
+      ];
+      EnvironmentFile = lib.optional (cfg.authTokenFile != null) cfg.authTokenFile;
+      StateDirectory = "silverbullet";
+    };
 
     services.nginx.virtualHosts."${cfg.domain}" = {
       enableACME = lib.mkDefault true;
@@ -40,9 +51,13 @@ in
       locations."/" = {
         proxyPass = "http://127.0.0.1:${toString cfg.port}";
         proxyWebsockets = true;
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
       };
     };
-
-    networking.firewall.allowedTCPPorts = [ cfg.port ];
   };
 }

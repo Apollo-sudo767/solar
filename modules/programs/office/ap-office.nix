f34099ca05ab004fae428b84281e3cde55240393
Apollo-sudo767 <fireshifter767@gmail.com>
2026-08-06 15:04:23 -0400

    refactor(ap-office): restrict ap-office suite to joplin, zotero, and pandoc

--- modules/programs/office/ap-office.nix
+++ modules/programs/office/ap-office.nix
@@ -11,7 +11,7 @@ let
 in
 {
   options.myFeatures.programs.office.ap-office = {
-    enable = lib.mkEnableOption "AP Office Suite (Joplin, Zotero, Pandoc, LibreOffice, Trilium)";
+    enable = lib.mkEnableOption "AP Office Suite (Joplin, Zotero, Pandoc)";
 
     joplin = lib.mkOption {
       type = lib.types.bool;
@@ -28,23 +28,11 @@ in
       default = true;
       description = "Include Pandoc & TeX Live publishing engine in AP Office Suite.";
     };
-    libreoffice = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Include LibreOffice suite in AP Office Suite.";
-    };
-    trilium = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Include Trilium Notes in AP Office Suite.";
-    };
   };
 
   config = lib.mkIf cfg.enable {
     myFeatures.programs.office.joplin.enable = lib.mkDefault cfg.joplin;
     myFeatures.programs.office.zotero.enable = lib.mkDefault cfg.zotero;
     myFeatures.programs.office.pandoc.enable = lib.mkDefault cfg.pandoc;
-    myFeatures.programs.office.libreoffice.enable = lib.mkDefault cfg.libreoffice;
-    myFeatures.programs.office.trilium.enable = lib.mkDefault cfg.trilium;
   };
 }

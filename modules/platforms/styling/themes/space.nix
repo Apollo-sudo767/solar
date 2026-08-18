8808c09e0df974203f6fe7db2990f9b4b81db71d
Apollo-sudo767 <fireshifter767@gmail.com>
2026-08-17 22:53:34 -0500

    fix(stylix): add explicit base16 schemes to themes to prevent IFD evaluation failures

--- modules/platforms/styling/themes/space.nix
+++ modules/platforms/styling/themes/space.nix
@@ -16,6 +16,24 @@ in
   config = lib.mkIf cfg.enable {
     myFeatures.platforms.styling.stylix = {
       enable = true;
+      scheme = {
+        base00 = "0f1419";
+        base01 = "131721";
+        base02 = "1e2430";
+        base03 = "242936";
+        base04 = "707a8c";
+        base05 = "b3b1ad";
+        base06 = "e6e1cf";
+        base07 = "f2eede";
+        base08 = "f29668";
+        base09 = "ff8f40";
+        base0A = "e6b450";
+        base0B = "c2d94c";
+        base0C = "95e6cb";
+        base0D = "59c2ff";
+        base0E = "d4bfff";
+        base0F = "e6b450";
+      };
       wallpaper = lib.mkDefault ../../../../assets/wallpapers/space.png;
     };
   };
